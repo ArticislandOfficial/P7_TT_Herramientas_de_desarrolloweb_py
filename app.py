1d0dbddb425a495b033a177583590f256969c7de
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2025-04-27 20:24:37 -0600

    App para streamlit

---
+++ app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st     
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header('Analiss de Vehivulos Proyecto 7')
+
+hist_button = st.button('Construir histograma') # crear un botón
+     
+if hist_button: # al hacer clic en el botón
+# escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+#___________________________________________
+
+# crear una casilla de verificación
+build_scatter = st.checkbox('Construir un diahrama de disperción')
+if build_scatter: # al hacer clic en el botón
+# escribir un mensaje
+    st.write('Construir un histograma para la columna odómetro')
+            
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)    
